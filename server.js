14796af73f39f05bed64121745ef40dd60f4d8c7
usuario <usuario.para.espacio1@gmail.com>
2019-07-01 01:30:52 -0500

    make it better

--- server.js
+++ server.js
@@ -23,8 +23,10 @@ app.use((req, res, next) => {
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-  res.send('Entrada principal')
+app.use(express.static(path.join(__dirname, '/client/ApreciacionEstudiantil')))
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname))
 })
 
 app.use(require('./routes'))
